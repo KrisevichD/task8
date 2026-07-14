7a4dadf414ca55751ad9db76b459878ab7e7ed33
denis <denis.krisevich@gmail.com>
2026-07-14 16:44:26 +0300

    setup: vitest + RTL

---
+++ components/button/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+import { Button } from './index'
+
+describe('Button Component', () => {
+  it('должен корректно рендерить текст кнопки', () => {
+    render(<Button>Кликни меня</Button>)
+    
+    const buttonElement = screen.getByText('Кликни меня')
+    expect(buttonElement).toBeInTheDocument()
+  })
+})
